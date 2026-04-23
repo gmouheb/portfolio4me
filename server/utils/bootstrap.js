5b713a02483334c2a97fa023ef8e39f3314c1c07
gmouheb <ghabrimouheb@gmail.com>
2026-04-23 21:59:49 +0100

    V1

--- server/utils/bootstrap.js
+++ server/utils/bootstrap.js
@@ -3,9 +3,25 @@ import { env } from "../config/env.js";
 import User from "../models/User.js";
 
 export async function ensureAdminUser() {
-  const existingUser = await User.findOne({ username: env.adminUsername }).lean();
+  const [userByUsername, userByEmail] = await Promise.all([
+    User.findOne({ username: env.adminUsername }),
+    User.findOne({ email: env.adminEmail }),
+  ]);
+
+  if (userByUsername && userByEmail && userByUsername.id !== userByEmail.id) {
+    throw new Error(
+      `Bootstrap admin conflict: username "${env.adminUsername}" and email "${env.adminEmail}" belong to different users`,
+    );
+  }
+
+  const existingUser = userByUsername || userByEmail;
 
   if (existingUser) {
+    existingUser.username = env.adminUsername;
+    existingUser.email = env.adminEmail;
+    existingUser.password = await bcrypt.hash(env.adminPassword, 10);
+    await existingUser.save();
+    console.log(`Updated bootstrap admin user "${env.adminUsername}"`);
     return;
   }
 
